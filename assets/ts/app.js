b558af785c1f328ad93a0244b8057f5d4a6ed9c1
Sogo Ohta <sogo.ohta@eure.jp>
2015-08-22 21:04:01 +0900

    First commit

---
+++ assets/ts/app.js
@@ -0,0 +1,10 @@
+/// <reference path="mithril.d.ts" />
+var m = require('mithril');
+function layout() {
+    return m("div", { class: "container" }, [
+        m("div", { id: "header" }, "This is the header!!!!"),
+        m("main", { id: "content" }, "This is the content"),
+        m("footer", { id: "footer" }, "This is the footer")
+    ]);
+}
+m.render(document.body, layout());
---
+++ assets/ts/app.ts
@@ -0,0 +1,13 @@
+/// <reference path="mithril.d.ts" />
+
+import m = require('mithril');
+
+function layout() {
+	return m("div", {class:"container"}, [
+		m("div", {id:"header"}, "This is the header!!!!"),
+		m("main", {id:"content"}, "This is the content"),
+		m("footer", {id:"footer"}, "This is the footer")
+	]);
+}
+
+m.render(document.body, layout());
