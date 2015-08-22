fce1a7db9020dafe397fba89712fbc02ff0d1c69
Sogo Ohta <sogo.ohta@eure.jp>
2015-08-22 21:44:14 +0900

    Clean package.json

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -5,7 +5,7 @@ import m = require('mithril');
 function layout() {
 	return m("div", {class:"container"}, [
 		m("div", {id:"header"}, "This is the header!!!!???"),
-		m("main", {id:"content"}, "This is the content"),
+		m("main", {id:"content"}, "This is the content!"),
 		m("footer", {id:"footer"}, "This is the footer")
 	]);
 }
