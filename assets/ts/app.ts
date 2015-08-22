48d00a17355decf75e9d2544d1c1fccd484ce725
Sogo Ohta <sogo.ohta@eure.jp>
2015-08-22 21:13:49 +0900

    Modify ignores

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -4,7 +4,7 @@ import m = require('mithril');
 
 function layout() {
 	return m("div", {class:"container"}, [
-		m("div", {id:"header"}, "This is the header!!!!"),
+		m("div", {id:"header"}, "This is the header!!!!???"),
 		m("main", {id:"content"}, "This is the content"),
 		m("footer", {id:"footer"}, "This is the footer")
 	]);
